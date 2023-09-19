458dba21f26dfcf84adfcccecb57f1d8e6c6a0be
mtolosaDisbyte <mtolosa@disbyte.com>
2023-09-19 18:59:41 -0300

    filas por pagina

--- src/body/body.jsx
+++ src/body/body.jsx
@@ -1,48 +1,53 @@
 import React, { useEffect, useState } from "react";
 import "./body.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BsTrash3Fill} from 'react-icons/bs'
-import {FaEdit} from 'react-icons/fa'
-import {BsInfoCircleFill} from 'react-icons/bs'
+import { BsTrash3Fill } from 'react-icons/bs'
+import { FaEdit } from 'react-icons/fa'
+import { BsInfoCircleFill } from 'react-icons/bs'
 import Pagination from 'react-bootstrap/Pagination';
 import {
-  Table 
+  Table
 } from "reactstrap";
 
 const Body = () => {
   const [colaboradores, setColaboradores] = useState([]);
   const [infoPage, setInfoPage] = useState({});
   const [itemPagination, setItemPagination] = useState([]);
- 
-    const GetList = (page, url) => { 
-        let uri =
-            page === null
-                ? url : `http://localhost:3001/api/list/?page=${page}`
+  const [pageSize, setPageSize] = useState(15); // Establece el valor inicial de registros por página
+  const [totalColaboradores, setTotalColaboradores] = useState(0); // Nuevo: Inicializa con 0
+
+
+  const GetList = (page, url) => {
+    let uri =
+      page === null
+        ? url : `http://localhost:3001/api/list/?page=${page}&pageSize=${pageSize}`; // Incluye el tamaño de la página en la solicitud
+
     fetch(uri)
       .then((response) => response.json())
       .then((data) => {
         setColaboradores(data.data);
         setInfoPage(data.info)
+        setTotalColaboradores(data.info.total);
       });
-    };
+  };
 
-    useEffect(() => {
-        GetList(0, null);
-    }, []);
+  useEffect(() => {
+    GetList(1, null); // Comienza con la primera página al cargar el componente
+  }, [pageSize]); // Añade pageSize como dependencia para actualizar cuando cambia
 
-    useEffect(() => {
-        let items = [];
-        for (let i = 1; i <= infoPage.pages; i++) { // Cambiado de infoPage.pages - 1 a infoPage.pages
-            items.push(
-                <Pagination.Item
-                    key={i}
-                    onClick={(e) => { GetList(parseInt(e.target.text), null) }}
-                >
-                    {i}
-                </Pagination.Item>)
-        }
-        setItemPagination(items);
-    }, [infoPage])
+  useEffect(() => {
+    let items = [];
+    for (let i = 1; i <= infoPage.totalPages; i++) {
+      items.push(
+        <Pagination.Item
+          key={i}
+          onClick={(e) => { GetList(parseInt(e.target.text), null) }}
+        >
+          {i}
+        </Pagination.Item>)
+    }
+    setItemPagination(items);
+  }, [infoPage.totalPages, pageSize]); // Añade pageSize como dependencia para actualizar cuando cambia
 
   return (
     <div className="body-div">
@@ -72,18 +77,34 @@ const Body = () => {
           ))}
         </tbody>
       </Table>
-      <div>
+      <div className="pagination">
         <Pagination>
-            <Pagination.Prev onClick={() => GetList(infoPage.prevPage)} />
-          {itemPagination.map((colaborador) => {
-            return colaborador;
+          <Pagination.Prev onClick={() => GetList(infoPage.prevPage)} />
+          {itemPagination.map((page) => {
+            return page;
           })}
-            <Pagination.Next onClick={() => GetList(infoPage.nextPage)} />
+          <Pagination.Next onClick={() => GetList(infoPage.nextPage)} />
         </Pagination>
+        <div className="lines-per-page">
+            {" "}
+            <select
+              className="selectan"
+              value={pageSize}
+              onChange={(e) => {
+                setPageSize(Number(e.target.value));
+              }}
+            >
+              {[15, 25, 35, 55, totalColaboradores].map((size) => (
+                <option key={size} value={size}>
+                  {size}
+                </option>
+              ))}
+            </select>{" "}
+          FILAS POR PAGINA
+          </div>
       </div>
     </div>
   );
 };
-
 
 export default Body;
